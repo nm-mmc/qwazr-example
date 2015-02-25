a75fdbbfd2b7728cf671249b83f0134283215659
nm-mmc <n.marietta@mesmateriaux.com>
2015-02-25 14:38:15 +0100

    Update index.groovy

--- controller/index.groovy
+++ controller/index.groovy
@@ -1,3 +1,3 @@
 //providers.cassandra.select("select * from repos", 1);
-response.variable("message", "Hello world!");
+response.variable("message", "Hello world! NICO");
 response.view("index.ftl");
